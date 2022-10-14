0804405a29074a707faf0552f8d5f2e0f1ee872d
Andrzej Swatowski <andrzejswatowski@outlook.com>
2022-10-14 09:56:36 +0200

    WIP: Nested Lookup Types

--- src/test/java/com/getindata/connectors/http/internal/table/lookup/HttpLookupTableSourceITCaseTest.java
+++ src/test/java/com/getindata/connectors/http/internal/table/lookup/HttpLookupTableSourceITCaseTest.java
@@ -234,6 +234,128 @@ public class HttpLookupTableSourceITCaseTest {
         );
     }
 
+    @Test
+    public void testNestedLookupJoinWithoutCast() throws Exception {
+        String fields =
+            "  `bool` BOOLEAN,\n" +
+            "  `tinyint` TINYINT,\n" +
+            "  `smallint` SMALLINT,\n" +
+            "  id INT,\n" +
+            "  `bigint` BIGINT,\n" +
+            "  `float` FLOAT,\n" +
+            "  `name` STRING,\n" +
+            "  `decimal` DECIMAL(9, 6),\n" +
+            "  `doubles` ARRAY<DOUBLE>,\n" +
+            "  `date` DATE,\n" +
+            "  `time` TIME(0),\n" +
+            "  `timestamp3` TIMESTAMP(3),\n" +
+            "  `timestamp9` TIMESTAMP(9),\n" +
+            "  `timestampWithLocalZone` TIMESTAMP_LTZ(9),\n" +
+            "  `map` MAP<STRING, BIGINT>,\n" +
+            "  `multiSet` MULTISET<STRING>,\n" +
+            "  `map2map` MAP<STRING, MAP<STRING, INT>>,\n" +
+            "  `row` ROW<`aStringColumn` STRING, `anIntColumn` INT, `aFloatColumn` FLOAT>,\n" +
+            "  `nestedRow` ROW<" +
+            "    `aStringColumn` STRING," +
+            "    `anIntColumn` INT," +
+            "    `aRow` ROW<`anotherStringColumn` STRING, `anotherIntColumn` INT>" +
+            "   >,\n" +
+            "  `aTable` ARRAY<ROW<" +
+            "      `aStringColumn` STRING," +
+            "      `anIntColumn` INT," +
+            "      `aFloatColumn` FLOAT" +
+            "  >>\n";
+
+        String sourceTable =
+            "CREATE TABLE Orders (\n"
+            + "  proc_time AS PROCTIME(),\n"
+            + fields
+            + ") WITH ("
+            + "'connector' = 'datagen',"
+            + "'rows-per-second' = '1',"
+            + "'fields.id.kind' = 'sequence',"
+            + "'fields.id.start' = '1',"
+            + "'fields.id.end' = '5'"
+            + ")";
+
+        String lookupTable =
+            "CREATE TABLE Customers (\n" +
+            "  `enrichedInt` INT,\n" +
+            "  `enrichedString` STRING,\n" +
+            "  \n"
+            + fields
+            + ") WITH ("
+            + "'format' = 'json',"
+            + "'connector' = 'rest-lookup',"
+            + "'url' = 'http://localhost:9090/client',"
+            + "'gid.connector.http.source.lookup.header.Content-Type' = 'application/json',"
+            + "'asyncPolling' = 'true'"
+            + ")";
+
+        tEnv.executeSql(sourceTable);
+        tEnv.executeSql(lookupTable);
+
+        // SQL query that performs JOIN on both tables.
+        String joinQuery =
+            "SELECT o.id, o.name, c.enrichedInt, c.enrichedString FROM Orders AS o"
+            + " JOIN Customers FOR SYSTEM_TIME AS OF o.proc_time AS c"
+            + " ON (\n"
+            + "  o.`bool` = c.`bool` AND\n"
+            + "  o.`tinyint` = c.`tinyint` AND\n"
+            + "  o.`smallint` = c.`smallint` AND\n"
+            + "  o.id = c.id AND\n"
+            + "  o.`bigint` = c.`bigint` AND\n"
+            + "  o.`float` = c.`float` AND\n"
+            + "  o.name = c.name AND\n"
+            + "  o.`decimal` = c.`decimal` AND\n"
+            + "  o.doubles = c.doubles AND\n"
+            + "  o.`date` = c.`date` AND\n"
+            + "  o.`time` = c.`time` AND\n"
+            + "  o.timestamp3 = c.timestamp3 AND\n"
+            + "  o.timestamp9 = c.timestamp9 AND\n"
+            + "  o.timestampWithLocalZone = c.timestampWithLocalZone AND\n"
+            + "  o.`map` = c.`map` AND\n"
+            + "  o.`multiSet` = c.`multiSet` AND\n"
+            + "  o.map2map = c.map2map AND\n"
+            + "  o.`row` = c.`row` AND\n"
+            + "  o.nestedRow = c.nestedRow AND\n"
+            + "  o.aTable = c.aTable\n"
+            + ")";
+
+        TableResult result = tEnv.executeSql(joinQuery);
+        result.await(15, TimeUnit.SECONDS);
+
+        // We want to have sort the result by "id" to make validation easier.
+        SortedSet<Row> collectedRows = new TreeSet<>(rowComparator);
+        try (CloseableIterator<Row> joinResult = result.collect()) {
+            while (joinResult.hasNext()) {
+                Row row = joinResult.next();
+                log.info("Collected row " + row);
+                collectedRows.add(row);
+            }
+        }
+
+        // validate every row and its column.
+        assertAll(() -> {
+                      assertThat(collectedRows.size()).isEqualTo(4);
+                      int intElement = 0;
+                      for (Row row : collectedRows) {
+                          intElement++;
+                          assertThat(row.getArity()).isEqualTo(6);
+
+                          // "id" nad "id2" columns should be different for every row.
+                          assertThat(row.getField("id")).isEqualTo(String.valueOf(intElement));
+                          assertThat(row.getField("id2")).isEqualTo(String.valueOf(intElement + 1));
+
+                          assertThat(row.getField("uuid"))
+                              .isEqualTo("fbb68a46-80a9-46da-9d40-314b5287079c");
+                          assertThat(row.getField("isActive")).isEqualTo(true);
+                          assertThat(row.getField("balance")).isEqualTo("$1,729.34");
+                      }
+                  }
+        );
+    }
+
     private void setupServerStub(WireMockServer wireMockServer) {
         StubMapping stubMapping = wireMockServer.stubFor(
             get(urlPathEqualTo(ENDPOINT))
